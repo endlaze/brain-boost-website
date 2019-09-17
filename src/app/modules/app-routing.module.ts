166226615fd63ea58e3868f4f8ff74ec25996603
Diego Velasquez <diesveca@hotmail.com>
2019-09-17 14:45:26 -0600

    Added home page

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from '../components/contact-component/contact.component'
 import { FAQComponent } from '../components/faq-component/faq.component'
+import { HomeComponent } from '../components/home-component/home.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/contact', pathMatch: 'full' },
   { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FAQComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' }
 ];
 
