c89389684f03b6311fa0a69a0b67632dfed5f975
Diego Velasquez <diesveca@hotmail.com>
2019-09-19 21:39:27 -0600

    Added home screen, multiple css and Bootsrap

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -5,7 +5,7 @@ import { FAQComponent } from '../components/faq-component/faq.component'
 import { HomeComponent } from '../components/home-component/home.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/contact', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'contact', component: ContactComponent },
   { path: 'faq', component: FAQComponent },
   { path: 'home', component: HomeComponent },
