bd074822977f9b8fb9c26dc4ebe25398591dd755
arcano50 <47094087+arcano50@users.noreply.github.com>
2019-11-06 15:08:25 -0600

    Update app.module.ts

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -9,12 +9,14 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 //Services
 import { ContactService } from '../services/contact-service/contact.service'
 import { FAQService } from '../services/faq-service/faq.service'
+import { ResetService } from '../services/reset-service/reset.service'
 //Components
 import { MainComponent } from '../components/main-component/main.component';
 import { ContactComponent } from '../components/contact-component/contact.component';
 import { FAQComponent } from '../components/faq-component/faq.component'
 import { NavComponent } from '../components/nav-component/nav.component';
 import { HomeComponent } from '../components/home-component/home.component'
+import { ResetComponent } from '../components/reset-component/reset.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { HomeComponent } from '../components/home-component/home.component'
     ContactComponent,
     FAQComponent,
     NavComponent,
+    ResetComponent,
     HomeComponent
   ],
   imports: [
@@ -32,7 +35,7 @@ import { HomeComponent } from '../components/home-component/home.component'
     HttpClientModule,
     FontAwesomeModule
   ],
-  providers: [ContactService, FAQService],
+  providers: [ContactService, FAQService, ResetService],
   bootstrap: [MainComponent]
 })
 export class AppModule {
